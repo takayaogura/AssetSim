7c3278830e8df62d5960f4b7178c32b80270722a
takayaogura <oguratakaya@icloud.com>
2021-05-01 13:38:24 +0900

    Initial Commit

---
+++ AssetSim/AssetSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetSimApp.swift
+//  AssetSim
+//
+//  Created by 小倉岳哉 on 2021/05/01.
+//
+
+import SwiftUI
+
+@main
+struct AssetSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
